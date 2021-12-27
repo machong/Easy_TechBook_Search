68db7c789f54177b0adaac8fa90bc31ebd984e31
My Name <example@example.com>
2021-12-27 16:26:04 +0000

    ターミナルにjsonを表示するところまで実装

--- app/controllers/get_exchange_rates_controller.rb
+++ app/controllers/get_exchange_rates_controller.rb
@@ -1,12 +1,11 @@
 class GetExchangeRatesController < ApplicationController
-  require 'net/http'
-  require 'uri'
+  require 'open-uri'
   require 'json'
   def getRate
-    uri = URI.parse('http://www.gaitameonline.com/rateaj/getrate')
-    http = Net::HTTP.new(uri.host, uri.port)
-    req = Net::HTTP::Get.new(uri.path)
-    res = http.request(req)
-    @result = JSON.parse(res.body)['quotes']
+    html = open('http://www.gaitameonline.com/rateaj/getrate').read
+    json = JSON.parse(html)['quotes']
+    json.each do |data|
+      puts "通貨:#{data['currencyPairCode']} 最高値:#{data['high']} 最安値:#{data['low']} ASK:#{data['ask']} BID:#{data['bid']} 開始値:#{data['open']}"
+    end
   end
 end
