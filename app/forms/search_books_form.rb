31a51340726b763497f8f94f56ce3c79c3535683
My Name <example@example.com>
2021-12-27 13:45:21 +0000

    Add function to access IT book.store api

--- app/forms/search_books_form.rb
+++ app/forms/search_books_form.rb
@@ -1,6 +1,25 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class SearchBooksForm
   include ActiveModel::Model
   include ActiveModel::Attributes
 
   attribute :keyword, :string
+
+  def query()
+    if !self.keyword.empty? then
+      keyword_mod = self.keyword.gsub(' ', '+')
+      uri = URI.parse(format('https://api.itbook.store/1.0/search/%<x>s', x: keyword_mod))
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      req = Net::HTTP::Get.new(uri.path)
+      res = https.request(req)
+      @hash = JSON.parse(res.body)
+    elsif
+      @hash = {books:[]}
+    end
+    return @hash
+  end
 end
