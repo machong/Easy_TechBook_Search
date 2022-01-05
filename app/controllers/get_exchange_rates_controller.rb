994a8976e5f271b187fb293b04c2c7c6b7a0df7b
My Name <example@example.com>
2022-01-05 04:51:09 +0000

    add rubocop and do first static code analysis

--- app/controllers/get_exchange_rates_controller.rb
+++ app/controllers/get_exchange_rates_controller.rb
@@ -1,6 +1,7 @@
 class GetExchangeRatesController < ApplicationController
   require 'open-uri'
   require 'json'
+# rubocop:disable all
   def getRate
     html = open('http://www.gaitameonline.com/rateaj/getrate').read
     json = JSON.parse(html)['quotes']
@@ -8,4 +9,5 @@ class GetExchangeRatesController < ApplicationController
       puts "通貨:#{data['currencyPairCode']} 最高値:#{data['high']} 最安値:#{data['low']} ASK:#{data['ask']} BID:#{data['bid']} 開始値:#{data['open']}"
     end
   end
+# rubocop:enable all
 end
--- app/forms/search_books_form.rb
+++ app/forms/search_books_form.rb
@@ -8,18 +8,18 @@ class SearchBooksForm
 
   attribute :keyword, :string
 
-  def query()
-    if !self.keyword.empty? then
-      keyword_mod = self.keyword.gsub(' ', '+')
+  def query
+    if !keyword.empty?
+      keyword_mod = keyword.gsub(' ', '+')
       uri = URI.parse(format('https://api.itbook.store/1.0/search/%<x>s', x: keyword_mod))
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       req = Net::HTTP::Get.new(uri.path)
       res = https.request(req)
       @hash = JSON.parse(res.body)
-    elsif
-      @hash = {books:[]}
+    elsif keyword.empty?
+      @hash = { books: [] }
     end
-    return @hash
+    @hash
   end
 end
