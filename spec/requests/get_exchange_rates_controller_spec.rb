a2e5184eec3570287b472dc37255b8f9a8e98dd1
My Name <example@example.com>
2022-01-06 20:54:02 +0000

    /get_exchange_rates/getRateのテストを追加

---
+++ spec/requests/get_exchange_rates_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe 'get_exchange_rates', type: :request do
+  describe "GET /get_exchange_rates/getRate" do
+    it "リクエストが成功すること" do
+      get '/get_exchange_rates/getRate'
+      expect(response).to have_http_status(200)
+    end
+  end
+end
