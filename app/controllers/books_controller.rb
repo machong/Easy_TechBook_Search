15f44cf2fab677bf0e672552c8eaf007f6a4dd32
My Name <example@example.com>
2022-01-08 02:39:40 +0000

    add nil check for @records

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,11 +8,11 @@ class BooksController < ApplicationController
     @search_form = SearchBooksForm.new(search_books_params)
 
     # IT BookstoreからのJSON戻り値を@recordsに格納する
-    unless params[:sort_by].nil? then
-      @records = @search_form.query().sort(sort_by: params[:sort_by], ascending: params[:ascending])["books"]
-    else      
-      @records = @search_form.query().sort()["books"]
-    end
+    @records = if params[:sort_by].nil?
+                 @search_form.query.sort['books']
+               else
+                 @search_form.query.sort(sort_by: params[:sort_by], ascending: params[:ascending])['books']
+               end
 
     # IT Bookstoreのサーバが落ちている時は↑の@records行をコメントアウトして↓のダミーデータを使う
     #
@@ -37,18 +37,15 @@ class BooksController < ApplicationController
 
     # IT BookstoreからのJSON戻り値(詳細版)を@detailed_recordsに格納する
     @detailed_records = []
-    @records.each do |book|
-      uri = URI.parse('https://api.itbook.store/1.0/books/' + book["isbn13"])
+    @records&.each do |book|
+      uri = URI.parse("https://api.itbook.store/1.0/books/#{book['isbn13']}")
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
       req = Net::HTTP::Get.new(uri.path)
       res = https.request(req)
       @detailed_records.push(JSON.parse(res.body))
     end
-
   end
-
-
 
   private
 
